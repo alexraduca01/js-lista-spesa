51806db861bcfee6296e5092b8b0db04782477f0
alexraduca01 <alex.raduca01@icloud.com>
2023-10-16 16:51:24 +0200

    while && for cicles done

--- js/script.js
+++ js/script.js
@@ -3,3 +3,23 @@
 // - Continuiamo a chiedere prodotti fino a quando l'utente non inserisce "stop"
 // MILESTONE 2 - STAMPA DELLA LISTA DELLA SPESA
 // Data la lista della spesa creata in precedenza, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente.
+
+const result = document.querySelector('div');
+const spesa = [];
+let ferma = false;
+
+while(ferma === false){
+
+    const richiesta = prompt("inserisci i prodotti da acquistare");
+    
+    if(richiesta === 'stop'){
+        ferma = true;
+    }else{
+        spesa.push(richiesta);
+    }
+
+}
+
+for(let a = 0; a < spesa.length; a++){
+    result.innerHTML = spesa;
+}
